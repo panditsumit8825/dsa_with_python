15d566e0776ee9b82d82d709a38de564f1e922e6
sumit kumar <panditsumit8825@gmail.com>
2026-09-04 23:51:09 +0530

    changes

--- patterns/patterns_3.py
+++ patterns/patterns_3.py
@@ -19,31 +19,10 @@
 #         print()
 # patterns6(6)
 
-# def patterns7(n):
-#     for i in range(n):
-#         print(" ",(n-i-1))
-#         for j in range(i):
-#             print("*")
-# patterns7(6)
-
-def selection_sort(arr):
-    n = len(arr)
-    
-    # Traverse through all array elements
+def patterns7(n):
     for i in range(n):
-        # Find the minimum element in remaining unsorted array
-        min_idx = i
-        for j in range(i + 1, n):
-            if arr[j] < arr[min_idx]:
-                min_idx = j
-                
-        # Swap the found minimum element with the first element
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
-        
-    return arr
+        print(" ",(n-i-1))
+        for j in range(i):
+            print("*")
+patterns7(6)
 
-# Example usage:
-numbers = [64, 25, 12, 22, 11]
-sorted_numbers = selection_sort(numbers)
-print("Sorted array:", sorted_numbers)
-# Output: Sorted array: [11, 12, 22, 25, 64]
