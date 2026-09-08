173957943ceac3a9794398b79a9ab9d9b9fb5dd5
sumit kumar <panditsumit8825@gmail.com>
2026-09-09 01:32:51 +0530

    changes

--- patterns/patterns_3.py
+++ patterns/patterns_3.py
@@ -52,7 +52,62 @@
 #         print()
 # pattern12(5)
 
-def pattern13(n):
-    for i in range(n):
-        print(i)
-pattern13(5)
+# def pattern13(n):
+#     num=1
+#     for i in range(1,n+1):
+#         # print(i)
+#         for j in range(1,i+1):
+#             print(num,end=" ")
+#             num = num + 1
+#         print()
+# pattern13(5)
+ 
+# def pattern14(n):
+#        for i in range(1,n+1):
+#         for j in range(i):
+#             print(chr(65+j),end=" ")
+#         print()
+# pattern14(5)
+
+# def pattern15(n):
+#     for i in range(n,0,-1):
+#         for j in range(i):
+#             print(chr(65+j),end=" ")
+#         print()
+# pattern15(5)
+
+# def pattern16(n):
+#     for i in range(n):
+#         for j in range(i+1):
+#             print(chr(65+i),end=" ")
+#         print()
+# pattern16(5)
+
+# def pattern17(n):
+#     for i in range(1,n):
+#         print(" "*(n-i),end="")
+#         for j in range(i):
+#             print(chr(65+j),end="")
+#         for j in range(i-2,-1,-1):
+#             print(chr(65+j),end="")
+#         print()
+# pattern17(5)
+
+# def pattern18(n):
+#     for i in range(n):
+#         start_ascii=ord('A') + n-1-i
+#         for j in range(i+1):
+#             print(chr(start_ascii+j),end=" ")
+#         print()
+# pattern18(5)
+
+# def pattern19(n):
+#     for i in range(1,n+1):
+#         for j in range(n):
+#             print("*"*(n-j)+" "*2*j+"*"*(n-j))
+#         for j in range(1,i+1):
+#             print("*"*j+" "*(2*n-2*j)+"*"*j)
+#         print()
+# pattern19(5)
+
+
