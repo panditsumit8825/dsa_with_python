e2def52401fd7d486624c5424e7f3db8d01aff8c
sumit kumar <panditsumit8825@gmail.com>
2026-09-12 22:27:38 +0530

    changes

--- basic_maths/recursion.py
+++ basic_maths/recursion.py
@@ -1,5 +1,35 @@
-arr = [1,2,3]
-for i in arr:
-    i=i*2
-print(arr)
-print(i)
+# # # arr = [1,2,3]
+# # # for i in arr:
+# # #     i=i*2
+# # # print(arr)
+# # # print(i)
+
+# # # Printing name n times by using recursion
+# # # def rec_name(n):
+# # #     if (n>0):
+# # #         print("Sumit")
+# # #         rec_name(n-1)
+# # # rec_name(15)
+
+# # # Printing 1 to N
+# # def rec_num(i,n):
+# #     if(i>n):
+# #         return 
+# #     else:
+# #         print(i)
+# #         # i+=1
+# #         rec_num(i+1,n)
+# # rec_num(1,5)
+
+# # Printing n to 1
+# def rev_num(n,i):
+#     if(n<i):
+#         return
+#     else:
+#         print(n)
+#         rev_num(n-1,i)
+# rev_num(5,1)
+
+# Print sum of first n numbers
+def rec_sum(i,n):
+    
