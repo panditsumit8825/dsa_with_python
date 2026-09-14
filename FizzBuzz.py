cbd50e7c9cbcdd0d877123d1a60f8447d4d65f75
sumit kumar <panditsumit8825@gmail.com>
2026-09-14 23:19:29 +0530

    fizzbuzz

---
+++ FizzBuzz.py
@@ -0,0 +1,15 @@
+n=int(input("Enter a number:"))
+str1="FizzBuzz"
+str2="Fizz"
+str3="Buzz"
+ans=[]
+for i in range(1,n+1):
+    if(i%3==0 and i%5==0):
+        ans.append(str1)
+    elif(i%3==0):
+        ans.append(str2)
+    elif(i%5==0):
+        ans.append(str3)
+    else:
+        ans.append(i)
+print(ans)
