370cae99a90f573dd5753f6d3fcae6a2d7e038ae
sumit kumar <panditsumit8825@gmail.com>
2026-09-18 18:29:29 +0530

    two sum

--- two_sum.py
+++ two_sum.py
@@ -2,6 +2,6 @@ list=[3,2,3]
 n=len(list)
 target=6
 for i in range(n-1):
-    l
-    if(list[i]+list[i+1]==target):
-        print([i,i+1])
+    for j in range(i+1,n):
+        if(list[i]+list[j]==target):
+            print([i,j])
