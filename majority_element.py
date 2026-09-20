df0494009e5eda13065b31572409f7aa6cbb66c1
sumit kumar <panditsumit8825@gmail.com>
2026-09-20 23:26:26 +0530

    majority element

---
+++ majority_element.py
@@ -0,0 +1,10 @@
+list=[2,2,1,1,1,2,2]
+n=len(list)
+count=0
+for i in range(n):
+    for j in range(1,n):
+        if(list[i]==list[j]):
+            count+=1
+            if(count>=n//2):
+                
+print(count)
