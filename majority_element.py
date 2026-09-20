b947bd7b5ffffb478e4f768fd1ecd37742b92c07
sumit kumar <panditsumit8825@gmail.com>
2026-09-20 23:28:17 +0530

    changes

--- majority_element.py
+++ majority_element.py
@@ -1,10 +1,10 @@
-list=[2,2,1,1,1,2,2]
+list=[2,2,9,7,4,3,4,2,2]
 n=len(list)
 count=0
 for i in range(n):
     for j in range(1,n):
         if(list[i]==list[j]):
             count+=1
-            if(count>=n//2):
+            # if(count>=n//2):
                 
 print(count)
