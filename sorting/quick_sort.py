39218383e1c44aeb797f772c0ceab3eabdef85ed
sumit kumar <panditsumit8825@gmail.com>
2026-09-24 00:05:34 +0530

    quick sort

--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -3,17 +3,23 @@ def partition(arr,low,high):
     i=low
     j=high
     while(i<j):
-        while(arr[i]<=arr[pivot] and i<=high):
+        while(arr[i]<=pivot and i<=high):
             i +=1
-        while(arr[j]>=arr[pivot] and j>=low):
+        while(arr[j]>pivot and j>=low):
             j -=1
         if(i<j):
-            arr[i]=arr[j]
-            
+            arr[i],arr[j] = arr[j],arr[i]
+    arr[low],arr[j]=arr[j],arr[low]
+    return j
 
-
-
+def quick_sort(arr,low=0,high=None):
+    if high is None:
+        high=len(arr)-1
+    if(low<high):
+        pivot_index=partition(arr,low,high)
+        quick_sort(arr,low,pivot_index-1)
+        quick_sort(arr,pivot_index+1,high)
 
 arr=[4,6,2,5,7,9,1,3]
-quick_sort(arr,0,len(arr)-1)
+quick_sort(arr)
 print("Sorted array :", arr)
