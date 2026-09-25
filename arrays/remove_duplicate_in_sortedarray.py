5a9d29ab3226b0d34a28762357485fa7ea5eba85
sumit kumar <panditsumit8825@gmail.com>
2026-09-26 00:34:44 +0530

    duplicates with negative value

--- arrays/remove_duplicate_in_sortedarray.py
+++ arrays/remove_duplicate_in_sortedarray.py
@@ -1,8 +1,8 @@
-arr=[1,1,2,2,2,3,3,4,4,4,5,5,5,6]
+arr=[-2,-3,1,1,2,2,2,3,3,4,4,4,]
 n=len(arr)
 i=0
 for j in range(1,n):
     if(arr[j]!=arr[i]):
         arr[i+1]=arr[j]
         i +=1
-print(arr[i])
+print(i+1)
